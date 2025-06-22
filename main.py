c3b9d9822d40c641e34be1aab99f8276ac3f56cc
nishantsoudai8755 <123gonishant@gmail.com>
2025-06-22 20:46:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,5 @@ def fulfillment_created():
     return "✅ Fulfillment received", 200
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))  # Render sets PORT env variable
+    app.run(host='0.0.0.0', port=port)
